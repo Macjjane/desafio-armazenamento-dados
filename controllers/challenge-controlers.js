837db6e77504633b2a56335b5c76a96cb083581c
macjjane <maidajane83@gmail.com>
2024-07-26 14:51:11 +0200

    update

---
+++ controllers/challenge-controlers.js
@@ -0,0 +1,60 @@
+const asyncHandler = require ("express-async-handler");
+
+
+
+const createUsers = asyncHandler(async (req, res) =>{
+ const createUsers = await user.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirmation: req.body.passwordConfirmation,
+    
+ });
+ res.status(201).json({ message: "Utilizador Criado com Sucesso!"})
+
+ if (password !== passwordConfirmation) errors.passwordConfirmation = "As senhas não coincidem.";
+
+ try {
+    const existingUser = await user.findOne({ email: email });
+    if (existingUser) {
+      errors.email = "O email já está em uso.";
+    }
+  } catch (error) {
+    return res.status(500).json({errors: { server: error.message } });
+  }
+});
+
+const createUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+   
+      const existingUser = await user.findOne({ email: email });
+      if (!existingUser) {
+        return res.status(404).json({ message: 'Utilizador não encontrado.' });
+      }
+  
+     
+      if (existingUser.password !== password) {
+        return res.status(401).json({ message: 'A password introduzida é inválida!' });
+      }
+  
+     
+      res.status(200).json({ message: 'Autenticação bem-sucedida.',  });
+    } catch (error) {
+      res.status(500).json({ errors: { server: error.message } });
+    }
+  });
+
+const getAllUsers = asyncHandler(async (req, res) => {
+ const users = await User.find();
+
+ res.status(200).json({ message: "Todos Users"})
+
+});
+
+const getUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    res.status(200).json({ message: "Obter User"})
+})
--- routes/challenge-routes.js
+++ routes/challenge-routes.js
@@ -1,10 +1,19 @@
-const express = require("express");
-
-
+const express = require("express")
+const{
+    createUsers,
+    createuser,
+    getAllUsers,
+    getUser
+} = require('../controllers/challenge-controlers')
 
 const router = express.Router()
 
-router.post("/", createUser);
-router.post("/:id", loginUser);
+router.use(express.json())
+
+router.post("/", createUsers);
+router.post("/:id", createuser);
 router.get("/", getAllUsers);
 router.get("/:id", getUser)
+
+
+module.exports = router;
